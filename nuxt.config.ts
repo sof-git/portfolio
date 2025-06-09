e2888729d66e6cea1b218a40aac5f7a8a581112d
sof <sofiane.turki01@gmail.com>
2025-06-09 16:20:05 +0200

    added configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['vuetify-nuxt-module'],
+  vuetify: {
+    vuetifyOptions: './vuetify.config.ts' // options file for Vuetify
+  }
 })
