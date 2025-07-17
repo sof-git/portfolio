d655bf43c49773abba31df1fbe4dfd39041e5502
sofiane-turki <sofiane.turki01@gmail.com>
2025-07-17 14:36:19 +0200

    updating timeline component for responsive design

--- pages/index.vue
+++ pages/index.vue
@@ -4,30 +4,32 @@ import Competences from '~/components/Competences.vue';
 import Experiences from '~/components/Experiences.vue';
 import Contact from '~/components/Contact.vue';
 </script>
+
 <template>
     <v-container class="pt-0">
         <v-row>
-            <v-col class="full-height-section" cols="12">
+            <v-col class="full-height-section" cols="12" id="bio">
                 <Bio />
             </v-col>
         </v-row>
         <v-row>
-            <v-col class="full-height-section" cols="12">
+            <v-col class="full-height-section" cols="12" id="competences">
                 <Competences />
             </v-col>
         </v-row>
         <v-row>
-            <v-col class="full-height-section" cols="12">
+            <v-col class="full-height-section" cols="12" id="experiences">
                 <Experiences />
             </v-col>
         </v-row>
         <v-row>
-            <v-col class="full-height-section" cols="12">
+            <v-col class="full-height-section" cols="12" id="contact">
                 <Contact />
             </v-col>
         </v-row>
     </v-container>
 </template>
+
 <style scoped>
 .full-height-section {
   min-height: 100vh;
